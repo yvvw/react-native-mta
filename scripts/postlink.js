103fb8150d74a1cba87b132922084468a39781b6
dongdayu <g592842897@gmail.com>
2018-04-26 18:24:51 +0800

    将修改 react native spec 文件部分移到 postinstall 脚本中，避免一定要 link 后才会生效

--- scripts/postlink.js
+++ scripts/postlink.js
@@ -1,22 +1,7 @@
 const path = require('path')
 const file = require('./file')
 
-const currentDir = path.dirname(__filename)
-const nodeModuleDir = currentDir + '/../../..'
-const projectDir = currentDir + '/../../../..'
+const projectDir = path.dirname(__filename) + '/../../../..'
 
-const modifies = {
-  get reactSpec () {
-    const avoidDupSign = '# Dependency'
-    return [
-      nodeModuleDir + '/react-native/React.podspec',
-      avoidDupSign + '\n  s.subspec "Dependency" do |ss|\n    ss.source_files         = "React/**/*.h"\n  end\n\n  ',
-      's.subspec "Core" do |ss|',
-      avoidDupSign
-    ]
-  }
-}
-
-file.fileInsert(...modifies.reactSpec)
 file.fileReplace(projectDir + '/android/settings.gradle', ':@yyyyu/react-native-mta', ':react-native-mta')
 file.fileReplace(projectDir + '/android/app/build.gradle', ':@yyyyu/react-native-mta', ':react-native-mta')
